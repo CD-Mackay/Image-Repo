54b8c04dabb75bd4b81c0b42175bfe4fbf00f1e6
CD-Mackay <connor.mackay@gmail.com>
2021-05-06 15:00:40 -0400

    create login form

--- src/Components/Logsign.jsx
+++ src/Components/Logsign.jsx
@@ -2,6 +2,17 @@ import React from 'react';
 
 export default function Logsign() {
   return (
-    
+    <div class="form-wrapper">
+    <form id="sign-up-form">
+      <input type="text" placeholder="enter username"></input>
+      <input type="password" placeholder="password"></input>
+      <button>Sign up!</button>
+    </form>
+    <form if="log-in-form">
+       <input type="text" placeholder="enter username"></input>
+      <input type="password" placeholder="password"></input>
+      <button>Sign up!</button>
+    </form>
+    </div>
   )
 }
