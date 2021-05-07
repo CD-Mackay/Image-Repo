d4a99a7f0672d818ac9cdf0c19e2de2d471c62e0
CD-Mackay <connor.mackay@gmail.com>
2021-05-07 12:12:26 -0400

    add react router to app

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import './App.css';
 import useApplicationData from './Hooks/useApplicationData';
 import Logsign from './Components/Logsign';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 function App() {
 
   
   const { file, fileName, saveFile, uploadFile, loginUser } = useApplicationData();
 
   return (
+    <Router>
     <div className="App">
-      <Logsign onLogin={loginUser} />
+      <Switch>
+        <Route path="/">
+          <Logsign onLogin={loginUser} />
+        </Route>
+        <Route path="/upload">
       <input type="file" onChange={saveFile} />
       <button onClick={uploadFile}>Upload</button>
+      </Route>
+      </Switch>
     </div>
+    </Router>
   );
 }
 
