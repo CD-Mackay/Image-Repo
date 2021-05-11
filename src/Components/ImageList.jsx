ee10381bf8efd932a59fd18414f5e58a456c5595
Connor Mackay <connor.mackay@gmail.com>
2021-05-11 16:11:49 -0400

    debug dynamic render

--- src/Components/ImageList.jsx
+++ src/Components/ImageList.jsx
@@ -2,13 +2,16 @@ import React, { useEffect, useState } from 'react';
 import useApplicationData from '../Hooks/useApplicationData';
 import ImageListItem from './ImageListItem';
 export default function ImageList(props) {
+let displayImage = <p>No images</p>
 
-  const displayImage = props.display.map(image => {
+if (props.display) {
+  displayImage = props.display.map(image => {
     return <ImageListItem id={image.id} key={image.id} name={image.name} path={image.file_path} />
   })
+}
   return (
     <div>
-      {/* { displayImage } */}
+      { displayImage }
     </div>
   )
 }
