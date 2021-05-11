9436c22940ed4cd7be937cfbd16d9b4978e9af68
Connor Mackay <connor.mackay@gmail.com>
2021-05-11 15:33:59 -0400

    debug dynamic render

--- src/Components/ImageListItem.jsx
+++ src/Components/ImageListItem.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 
 export default function ImageListItem(props) {
   return (
+    <div>
     <p>{props.name}</p>
+    <img src={props.file_path} />
+    </div>
   )
 }
--- src/Hooks/useApplicationData.js
+++ src/Hooks/useApplicationData.js
@@ -13,7 +13,6 @@ export default function useApplicationData() {
 
   useEffect(() => {
     getAllImages();
-
   }, []);
 
   function getAllImages() {
