13c650cba12bd90a80c2febf5ba3a172089c980e
Connor Mackay <connor.mackay@gmail.com>
2021-05-18 12:58:28 -0400

    Add content to header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import './headerstyles.css';
 
 export default function Header() {
   return (
-    <h1>I am header</h1>
+    <div className="header">
+      <div className="header-wrap">
+      <h1>Foto Album!</h1>
+      </div>
+      <div className="info-wrap">
+        <ul>
+          <li>Upload images!</li>
+          <li>View and sort your saved images!</li>
+        </ul>
+      </div>
+    </div>
   )
 }
