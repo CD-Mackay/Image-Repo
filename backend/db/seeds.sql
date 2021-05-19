20dd68fdc29ed621055c7aeb5b7f6a6dc659372b
Connor Mackay <connor.mackay@gmail.com>
2021-05-19 10:12:19 -0400

    add favourite boolean to database

--- backend/db/seeds.sql
+++ backend/db/seeds.sql
@@ -1,3 +1,9 @@
 INSERT INTO users (name, password_digest) VALUES 
 ('connor', 'password'),
 ('frank', 'password');
+
+INSERT INTO images (name, user_id, file_path, date_added) VALUES
+('bitefish.gif', 1, '/Users/connormackay/practice/image-repo/public/', '1621356030051'),
+('compassprofile.jpg', 1, '/Users/connormackay/practice/image-repo/public/', '1621356035865'),
+('Webprepprofile.jpg', 1, '/Users/connormackay/practice/image-repo/public/', '1621356047701');
+
