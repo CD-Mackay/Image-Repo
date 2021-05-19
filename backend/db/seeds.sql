67336f56b8f79adf0f46dbcdba72a4f9380ba732
Connor Mackay <connor.mackay@gmail.com>
2021-05-19 10:44:33 -0400

    add more seeds for testing

--- backend/db/seeds.sql
+++ backend/db/seeds.sql
@@ -2,8 +2,9 @@ INSERT INTO users (name, password_digest) VALUES
 ('connor', 'password'),
 ('frank', 'password');
 
-INSERT INTO images (name, user_id, file_path, date_added) VALUES
-('bitefish.gif', 1, '/Users/connormackay/practice/image-repo/public/', '1621356030051'),
-('compassprofile.jpg', 1, '/Users/connormackay/practice/image-repo/public/', '1621356035865'),
-('Webprepprofile.jpg', 1, '/Users/connormackay/practice/image-repo/public/', '1621356047701');
+INSERT INTO images (name, user_id, file_path, date_added, favourite) VALUES
+('bitefish.gif', 1, '/Users/connormackay/practice/image-repo/public/', '1621356030051', false),
+('compassprofile.jpg', 1, '/Users/connormackay/practice/image-repo/public/', '1621356035865', false),
+('Webprepprofile.jpg', 1, '/Users/connormackay/practice/image-repo/public/', '1621356047701', true),
+('babu!.jpg', 1, '/Users/connormackay/practice/image-repo/public/', '1621356047701', true);
 
