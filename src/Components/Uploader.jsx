4207e106e75651d91ad7bcaacb4df217cbbd4cf6
Connor Mackay <connor.mackay@gmail.com>
2021-05-24 16:15:03 -0400

    style input button

--- src/Components/Uploader.jsx
+++ src/Components/Uploader.jsx
@@ -1,16 +1,17 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import LogOutButton from './LogOutButton';
 import './buttonstyles.css';
 
 
 export default function Uploader(props) {
 
+
   const fileInput = useRef(null);
 
   return (
     <div className="upload-wrapper">
       <h4>Upload new Image</h4>
-      <button className="select-button" onClick={() => fileInput.current.click()}>Choose File</button>
+      <button className="upload-button" onClick={() => fileInput.current.click()}>Choose File</button>
       <input type="file" ref={fileInput} onChange={props.save} style={{ display: 'none' }}/>
       <button className="upload-button" onClick={props.upload}>Upload</button>
     </div>
