bec81f25d9a11fb4ca72c75376ed1aab15341797
Connor Mackay <connor.mackay@gmail.com>
2021-06-01 11:34:43 -0400

    add further logsign tests

--- src/Components/__tests__/Logsign.test.js
+++ src/Components/__tests__/Logsign.test.js
@@ -12,5 +12,13 @@ describe("Logsign", () => {
     const { getByText, getByPlaceholderText } = render(<Logsign />);
     fireEvent.click(getByText("New Here? Sign up!"));
     expect(getByPlaceholderText("pick a secure password")).toBeInTheDocument;
-  })
+  });
+
+  it("cycles between login and signup forms", () => {
+    const { getByText, getByPlaceholderText } = render(<Logsign />);
+    fireEvent.click(getByText("New Here? Sign up!"));
+    expect(getByPlaceholderText("pick a secure password")).toBeInTheDocument;
+    fireEvent.click(getByText("Click here to Login!"));
+    expect(getByPlaceholderText("enter username")).toBeInTheDocument;
+  });
 });
