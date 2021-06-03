778c151408b352eedb03b07950e0f34fd7fcfd3d
Connor Mackay <connor.mackay@gmail.com>
2021-06-03 13:07:38 -0400

    style uploader component

--- src/Components/Uploader.jsx
+++ src/Components/Uploader.jsx
@@ -11,9 +11,11 @@ export default function Uploader(props) {
   return (
     <div className="upload-wrapper">
       <h4>Upload new Image</h4>
+      <div className="up-control-wrap">
       <button className="upload-button" onClick={() => fileInput.current.click()}>Choose File</button>
       <input type="file" ref={fileInput} onChange={props.save} style={{ display: 'none' }}/>
       <button className="upload-button" onClick={props.upload}>Upload</button>
+      </div>
     </div>
   )
 }
